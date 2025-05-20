ec8c1094d352c3d1403c1582b07c5592aa66902b
IsaqueGarcia <isaqueemprego1@gmail.com>
2025-05-20 00:29:38 -0300

    feat: linked list leetcode

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -60,7 +60,7 @@ public class LinkedList {
     public static class Node {
 
         public int value;
-        Node next;
+        public Node next;
 
         public Node(int value){
             this.value = value;
