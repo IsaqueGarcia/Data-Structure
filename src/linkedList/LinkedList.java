8d5f3e55e407f2d577ba8dbbbe5f998db862ad50
IsaqueGarcia <isaqueemprego1@gmail.com>
2025-05-21 23:08:41 -0300

    feat: swap pairs and reverse between

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -8,43 +8,46 @@ public class LinkedList {
         LinkedList linkedList = new LinkedList(1);
 
         linkedList.append(2);
-        linkedList.append(5);
-        linkedList.append(23);
-        linkedList.append(40);
-        linkedList.printList();
+        linkedList.append(3);
+        linkedList.append(4);
 
-        System.out.println("=======After remove========");
-        linkedList.removeLast();
         linkedList.printList();
-
-        System.out.println("=======After include a new node in the begin========");
-        linkedList.prepend(15);
+        linkedList.swapPairs();
         linkedList.printList();
-
-        System.out.println("=======After remove the first node========");
-        linkedList.removeFirst();
-        linkedList.printList();
-
-        System.out.println("=======Get by index========");
-        System.out.println("Index (2), value: " + linkedList.get(2).value);
-
-        System.out.println("=======Set by index========");
-        linkedList.set(2,10);
-        linkedList.printList();
-
-        System.out.println("=======Insert========");
-        linkedList.insert(0, 33);
-        linkedList.insert(linkedList.getLength(), 44);
-        linkedList.insert(3,55);
-        linkedList.printList();
-
-        System.out.println("=======Remove========");
-        linkedList.remove(linkedList.getLength() - 1);
-        linkedList.printList();
-
-        System.out.println("=======Reverse========");
-        linkedList.reverse();
-        linkedList.printList();
+//        linkedList.printList();
+//
+//        System.out.println("=======After remove========");
+//        linkedList.removeLast();
+//        linkedList.printList();
+//
+//        System.out.println("=======After include a new node in the begin========");
+//        linkedList.prepend(15);
+//        linkedList.printList();
+//
+//        System.out.println("=======After remove the first node========");
+//        linkedList.removeFirst();
+//        linkedList.printList();
+//
+//        System.out.println("=======Get by index========");
+//        System.out.println("Index (2), value: " + linkedList.get(2).value);
+//
+//        System.out.println("=======Set by index========");
+//        linkedList.set(2,10);
+//        linkedList.printList();
+//
+//        System.out.println("=======Insert========");
+//        linkedList.insert(0, 33);
+//        linkedList.insert(linkedList.getLength(), 44);
+//        linkedList.insert(3,55);
+//        linkedList.printList();
+//
+//        System.out.println("=======Remove========");
+//        linkedList.remove(linkedList.getLength() - 1);
+//        linkedList.printList();
+//
+//        System.out.println("=======Reverse========");
+//        linkedList.reverse();
+//        linkedList.printList();
     }
 
 
@@ -252,6 +255,63 @@ public class LinkedList {
             start = start.next;
         }
 
+
+    }
+
+    public void reverseBetween(int m, int n){
+
+        Node dummy = new Node(0);
+        dummy.next = head;
+
+        Node prev = dummy;
+        Node current = head;
+        Node toMove = current.next;
+
+        for(int i = 0; i < m; i++){ //Move the pointer to start revert
+            current = current.next;
+            prev = prev.next;
+            toMove = toMove.next;
+        }
+
+        for(int i = m; i < n; i++){
+            current.next = toMove.next;
+            toMove.next = prev.next;
+            prev.next = toMove;
+
+            //current = current.next;
+            toMove = current.next;
+        }
+
+        head = dummy.next;
+
+    }
+
+    public void swapPairs() {
+
+        if(head == null) return;
+
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node prev = dummy;
+
+        Node first = head;
+
+        //1 - 2 - 3 - 4
+
+        while(first != null && first.next != null){
+
+            Node second = first.next;
+
+            prev.next = second; //2 / 4
+            first.next = second.next; //3 / null
+            second.next = first; //1 / 3
+
+            prev = first; //in the same point
+            first = first.next; //move to the next first
+
+        }
+
+        head = dummy.next;
 
     }
 
---
+++ src/linkedList/leetcode/reverseBetween.java
@@ -0,0 +1,38 @@
+package linkedList.leetcode;
+
+public class reverseBetween {
+
+//    public void reverseBetween(int m, int n){
+//
+//        Node dummy = new Node(0);
+//        dummy.next = head;
+//
+//        Node prev = dummy;
+//        Node current = head;
+//
+//        if(current == null){
+//            return;
+//        }
+//
+//        Node toMove = current.next;
+//
+//        for(int i = 0; i < m; i++){ //Move the pointer to start revert
+//            current = current.next;
+//            prev = prev.next;
+//            toMove = toMove.next;
+//        }
+//
+//        for(int i = m; i < n; i++){
+//            current.next = toMove.next;
+//            toMove.next = prev.next;
+//            prev.next = toMove;
+//
+//
+//            toMove = current.next;
+//        }
+//
+//        head = dummy.next;
+//
+//    }
+
+}
